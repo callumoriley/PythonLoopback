ef8968fa986dc63f320bff8e462590bddf7698ab
Callum O'Riley <callumchristopheroriley@gmail.com>
2022-07-03 23:34:48 -0700

    Added version requirement in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     name="PythonLoopback",
     version="0.1",
     description="Allows Python to get information about audio currently playing on the system",
+    python_requires="==3.7.9",
     ext_modules=[pl_module]
 )
