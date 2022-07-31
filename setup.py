e330bca5c3fc14979a469f23c04079da351b2365
Callum O'Riley <86801140+callumoriley@users.noreply.github.com>
2022-07-30 21:47:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,24 @@ pl_module = Extension("PythonLoopback", sources=["PythonLoopback.cpp"], include_
 
 setup(
     name="PythonLoopback",
+    packages=["PythonLoopback"],
     version="0.3",
+    license="GPLv3",
     description="Allows Python to get information about audio currently playing on the system",
+    author="Callum O'Riley",
+    author_email="callumchristopheroriley@gmail.com",
+    url="https://github.com/callumoriley/PythonLoopback",
+    keywords=["windows", "audio", "loopback"]
     python_requires=">=3.10.0",
     platforms=["Windows"],
-    ext_modules=[pl_module]
+    ext_modules=[pl_module],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GPLv3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.10',
+  ],
 )
