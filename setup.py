b159edde592d769b838d7ce717e10ef18850090e
Callum O'Riley <callumchristopheroriley@gmail.com>
2022-08-15 21:39:23 -0700

    Changes to make package work with PyPI

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ pl_module = Extension("PythonLoopback", sources=["PythonLoopback.cpp"], include_
 
 setup(
     name="PythonLoopback",
-    version="0.3",
+    version="0.3-alpha",
     license="GPLv3",
     description="Allows Python to get information about audio currently playing on the system",
     author="Callum O'Riley",
     author_email="callumchristopheroriley@gmail.com",
     url="https://github.com/callumoriley/PythonLoopback",
+    download_url="https://github.com/callumoriley/PythonLoopback/archive/refs/tags/v0.3.tar.gz",
     keywords=["windows", "audio", "loopback"],
     python_requires=">=3.10.0",
     platforms=["Windows"],
@@ -20,7 +21,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GPLv3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.10',
